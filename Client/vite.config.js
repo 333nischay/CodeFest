38ea96b9cf39ac8828cb44fb6f7bdf0a6bb2fba1
nirved.mishra.eee23@itbhu.ac.in <nirved431@gmail.com>
2025-02-06 01:54:22 +0530

    added vercel.json

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: './',
+  build: {
+    outDir: 'dist', // Default, but make sure it's correct
+    assetsDir: 'assets',
+  },
 })
